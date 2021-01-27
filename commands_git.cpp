659356f1af93ec07d199240be32f450f12bef6a2
Franks Jhon Colque Zegarra <czfranks@gmail.com>
2021-01-27 16:58:59 -0500

    Resumiendo los commandos principales de GIT
    Primeros comandos usados!!

---
+++ commands_git.cpp
@@ -0,0 +1,18 @@
+COMANDOS DE GIT - RESUMEN
+
+===============
+CONFIGURACION
+===============
+//configurar el nombre & email del usuario
+git config --global user.name "mi nombre"
+git config --global user.email "mi email"
+
+//listar las configuraciones
+git config --list
+
+===============
+STAGGING AREA
+===============
+//verificar el estatus de git( archivos nuevos, editados, borrados, etc)
+git status
+
