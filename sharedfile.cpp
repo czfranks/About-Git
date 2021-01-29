c6512add6e50c30bc5ad77490f07466036d72b41
gitczfranks <czfranks@gmail.com>
2021-01-29 13:20:07 -0500

    Agregando contenido a la seccion que me corresponde

--- sharedfile.cpp
+++ sharedfile.cpp
@@ -1,6 +1,6 @@
 ===============FRANKS MODIFICA ESTA PARTE DEL ARCHIVO========
 
-
+Hago estas moficaciones en mi parte del archivo que me corresponde.
 
 =============================================================
 
